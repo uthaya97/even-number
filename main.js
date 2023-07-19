1de54fef68632d0bc8354ed110319e25cc808e79
paulkani <paulkani.s@golden-hippo.com>
2023-07-19 19:41:45 +0530

    even

---
+++ main.js
@@ -0,0 +1,10 @@
+var arr=[10,13,20,25,40,73,60,19,80];
+document.write("given number"+" "+arr+"<br>"+"<br>");
+for(let i=0;i<arr.length;i++)
+            {
+			if(arr[i]%2==0)
+			{
+			document.write(arr[i]);
+			document.write("<br>");
+			}
+			}
